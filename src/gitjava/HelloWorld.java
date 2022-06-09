817fe03dd4ff9d0f9e43c3c7429305f00e225e17
jay_l <jay_l@LAPTOP-57721RJI>
2022-06-08 21:25:16 -0500

    New Changes

--- src/gitjava/HelloWorld.java
+++ src/gitjava/HelloWorld.java
@@ -5,7 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello there");
 		System.out.println("This is second line");
-		System.out.println("This is third line");
+		System.out.println("This is third line ");
+		System.out.println("This is fourth line ");
 	}
 
 }
--- src/gitjava/ShapeArea.java
+++ src/gitjava/ShapeArea.java
@@ -2,6 +2,6 @@ package gitjava;
 
 public class ShapeArea {
 public static void main(String[] args) {
-	System.out.println("hi there");
+	System.out.println(" shape class: hi there");
 }
 }
